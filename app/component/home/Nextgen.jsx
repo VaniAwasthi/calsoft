e22b421b5c79b2ea95f18fb8452b8295042ab911
Vani Awasthi <awasthivani95@gmail.com>
2025-04-05 12:47:51 +0530

    landing-page-full

--- app/component/home/Nextgen.jsx
+++ app/component/home/Nextgen.jsx
@@ -28,10 +28,10 @@ const Nextgen = () => {
         <div className="mx-auto px-4 xl:px-20" id="customers">
           {/* icon-with heading*/}
           <div className="flex gap-6 md:gap-6 items-center mb-6 py-5">
-            <div className="rounded-full bg-[#ffff] w-8 h-8 p-2 flex items-center justify-center">
+            <div className="rounded-full bg-[#ffff] w-8 h-8 p-2 flex items-center justify-center my-2">
               <Image src={icon} alt="icon" width={16} height={16} />
             </div>
-            <p className="font-light text-xl text-white">Industry</p>
+            <p className="font-light text-xl text-white my-2">Industry</p>
           </div>
           <div className="container mx-auto px-4 xl:px-20">
             {/* tab */}
@@ -74,14 +74,15 @@ const Nextgen = () => {
               initial={{ x: 50, opacity: 0 }}
               animate={{ x: 2, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.2 }}
-              className="flex lg:flex-row flex-row mt-[4rem] mb-[2rem]"
+              className="grid grid-cols-1 lg:grid-cols-12 gap-8 mt-[4rem] mb-[2rem]"
             >
               {/* Image */}
               {activeIndustry && (
                 <motion.div
                   initial={{ y: 50, opacity: 0 }}
-                  animate={{ y: 2, opacity: 1 }}
+                  animate={{ y: 4, opacity: 1 }}
                   transition={{ duration: 0.5, delay: 0.2 }}
+                  className="lg:col-span-4"
                 >
                   <Image
                     src={activeIndustry.image}
@@ -93,7 +94,7 @@ const Nextgen = () => {
                 </motion.div>
               )}
               {/* content */}
-              <div className="ml-[4rem] text-white">
+              <div className="ml-[4rem] text-white  lg:col-span-8">
                 {activeIndustry && (
                   <>
                     <motion.h2
@@ -107,7 +108,7 @@ const Nextgen = () => {
                     <motion.p
                       initial={{ y: 50, opacity: 0 }}
                       animate={{ y: 2, opacity: 1 }}
-                      transition={{ duration: 0.5, delay: 0.4 }}
+                      transition={{ duration: 0.5, delay: 0.6 }}
                       className="py-3 text-lg"
                     >
                       {activeIndustry.description}
@@ -126,50 +127,57 @@ const Nextgen = () => {
                         <span className="absolute inset-0 bg-white w-full h-full left-0 -translate-x-full transition-transform duration-500 ease-out group-hover:translate-x-0"></span>
                       </button>
                       <button
-                        className="relative flex gap-2 justify-between items-center  px-8 py-4 rounded-full shadow-md bg-white text-[#E8282B] transition-all duration-500 overflow-hidden group"
+                        className="relative flex gap-2 justify-between items-center bg-transparent border-1 border-white  px-8 py-4 rounded-full shadow-md text-white transition-all duration-500 overflow-hidden group"
                         style={{
                           boxShadow: "0px 12px 16px rgba(0, 54, 19, 0.25",
                         }}
                       >
-                        <span className="relative z-10 group-hover:text-white w-full flex gap-4 items-center transition-colors duration-500">
+                        <span className="relative z-10 group-hover:text-[#E8282B] w-full flex gap-4 items-center transition-colors duration-500">
                           know More
-                          <IoArrowForwardCircleSharp className="text-3xl tra" />
                         </span>
-                        <span className="absolute inset-0 bg-[#E8282B] w-full h-full left-0 -translate-x-full transition-transform duration-500 ease-out group-hover:translate-x-0"></span>
+                        <span className="absolute inset-0 bg-white w-full h-full left-0 -translate-x-full transition-transform duration-500 ease-out group-hover:translate-x-0"></span>
                       </button>
                     </div>
                   </>
                 )}
 
-<div className="flex flex-col justify-between mt-16 space-y-6">
-      <div className="flex flex-wrap gap-6">
-        {activeIndustry?.iconData.map((card) => (
-          <div
-            key={card.id}
-            className="card-hover w-24 h-24 perspective cursor-pointer"
-          >
-            <div className="card-inner relative w-full h-full">
-              {/* Front Side */}
-              <div className="absolute inset-0 flex items-center justify-center bg-gray-300 text-black border border-gray-400 rounded-lg shadow-md backface-hidden">
-                <Image
-                  src={card.icon}
-                  alt={card.title}
-                  width={40}
-                  height={40}
-                />
-              </div>
+                <motion.div
+                  initial={{ y: 50, opacity: 0 }}
+                  animate={{ y: 4, opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.9 }}
+                  className="flex flex-col justify-between mt-16 space-y-6"
+                >
+                  <div className="flex flex-wrap gap-6">
+                    {activeIndustry?.iconData.map((card) => (
+                      <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={{ y: 4, opacity: 1 }}
+                        transition={{ duration: 0.8, delay: 0.9 }}
+                        key={card.id}
+                        className="card-hover w-[100px] h-[110px] hover:h-[120px] hover:w-[105px] perspective cursor-pointer"
+                      >
+                        <div className="card-inner relative w-full h-full">
+                          {/* Front Side */}
+                          <div className="absolute inset-0 flex items-center justify-center bg-[#FFFFFF] text-black border border-[#FFFFFF] rounded-lg shadow-md backface-hidden">
+                            <Image
+                              src={card.icon}
+                              alt={card.title}
+                              width={40}
+                              height={40}
+                            />
+                          </div>
 
-              {/* Back Side */}
-              <div className="absolute inset-0 flex items-center justify-center bg-blue-500 text-white rounded-lg rotate-y-180 backface-hidden">
-                <p className="text-sm font-semibold text-center px-2">
-                  {card.title}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+                          {/* Back Side */}
+                          <div className="absolute inset-0 flex items-center justify-center bg-[#FFFFFF] text-[#021553] rounded-lg rotate-y-180 backface-hidden">
+                            <p className="text-[12px] font-semibold text-center px-2">
+                              {card.title}
+                            </p>
+                          </div>
+                        </div>
+                      </motion.div>
+                    ))}
+                  </div>
+                </motion.div>
               </div>
             </motion.div>
           </div>
--- app/page.js
+++ app/page.js
@@ -1,21 +1,24 @@
 "use client";
+import {
+  CalsoftNews,
+  CalsoftPartner,
+} from "./component/home/CalsoftNewsPartner";
 import Customer from "./component/home/Customer";
 import DigitalJounery from "./component/home/DigitalJourney";
 import HeroSec from "./component/home/HeroSec";
 import Nextgen from "./component/home/Nextgen";
 import Image from "next/image";
-import { CalsoftNews, CalsoftPartner } from "./screen/home/CalsoftNewsPartner";
-import { Casestudy, Services } from "./screen/home/ServicesCasestudyBlog";
+import { Casestudy, Services } from "./component/home/ServicesCasestudyBlog";
 
 export default function Home() {
   return (
     <>
       <HeroSec />
       <DigitalJounery />
-       <Customer/>
+      <Customer />
       <Nextgen />
       <CalsoftNews />
-      <CalsoftPartner/>
+      <CalsoftPartner />
       <Services />
       <Casestudy />
     </>
