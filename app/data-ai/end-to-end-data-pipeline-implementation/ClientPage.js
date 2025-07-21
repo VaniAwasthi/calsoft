4cc525c119304e652caef8526c41ec3134637748
Swanand Shinge <swanand.shinge@calsoftinc.com>
2025-07-21 17:27:02 +0530

    Add SEO title, meta for Data & AI pages also add H1

--- app/data-ai/end-to-end-data-pipeline-implementation/ClientPage.js
+++ app/data-ai/end-to-end-data-pipeline-implementation/ClientPage.js
@@ -22,14 +22,6 @@ const ClientPage = () => {
   return (
     <>
 
-      <Seo
-        title="End-to-End Data Pipeline Implementation Services | CalSoft Inc."
-        description="Accelerate insights with our end-to-end data pipeline implementation—covering ETL, real-time and cloud pipelines for robust, scalable data solutions."
-        keywords="End-to-end data pipeline implementation"
-        url="/data-ai/end-to-end-data-pipeline-implementation"
-        image="http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInfoSec4.84a355ab.webp&w=1080&q=75"
-      />
-    
       <HeroSectionDatapipelineengineering />
       <ChallengeSolutionSec />
       <DataArchitectureSec />
