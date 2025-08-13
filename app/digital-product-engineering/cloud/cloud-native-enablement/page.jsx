511082b8962933a79bac39d2f8b156c6158e71bc
Vani Awasthi <awasthivani95@gmail.com>
2025-08-13 16:28:26 +0530

    linking

--- app/digital-product-engineering/cloud/cloud-native-enablement/page.jsx
+++ app/digital-product-engineering/cloud/cloud-native-enablement/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import HeroSec from "../../component/infra/HeroSec";
-import WhyItMatters from "../../component/infra/Cloud-Native-Enablement/WhyItMatters";
-import IntelligentPlanning from "../../component/infra/Multi-Cloud-Provisioning/IntelligentPlanning";
-import UseCases from "../../component/infra/UseCases";
-import FirstStep from "../../component/infra/FirstStep";
-import WhyCalsoft from "../../component/infra/WhyCalsoft";
+import HeroSec from "../../../component/infra/HeroSec";
+import WhyItMatters from "../../../component/infra/Cloud-Native-Enablement/WhyItMatters";
+import IntelligentPlanning from "../../../component/infra/Multi-Cloud-Provisioning/IntelligentPlanning";
+import UseCases from "../../../component/infra/UseCases";
+import FirstStep from "../../../component/infra/FirstStep";
+import WhyCalsoft from "../../../component/infra/WhyCalsoft";
 
 export const Page = () => {
   return (
