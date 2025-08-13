b52d5ee1f63ffea22f8ca466d0b5713734343b5d
Vani Awasthi <awasthivani95@gmail.com>
2025-08-13 17:06:54 +0530

    linking

--- app/digital-product-engineering/modernization-ecosystem/future-readiness-with-application-modernization/page.js
+++ app/digital-product-engineering/modernization-ecosystem/future-readiness-with-application-modernization/page.js
@@ -1,45 +1,49 @@
-import React from 'react'
-import { Business, Future, How, Steps, Use, What, Why } from '../../../component/FutureReadiness/FutureWhyWhatUseBusinessHowSteps'
+import React from "react";
+import {
+  Business,
+  Future,
+  How,
+  Steps,
+  Use,
+  What,
+  Why,
+} from "../../../component/FutureReadiness/FutureWhyWhatUseBusinessHowSteps";
 import ButtonImage from "../../../assets/home/buttonImg.webp";
-import { BannerSection } from '../../../component/utilities/InfraSectorSec';
+import { BannerSection } from "../../../component/utilities/InfraSectorSec";
 import BannerwithButtonBg from "../../../assets/DigitalEngineering/Product/BannerwithButton.webp";
-import { FooterBg } from '../../../component/utilities/DoCases';
+import { FooterBg } from "../../../component/utilities/DoCases";
 import ContactImg from "../../../assets/Data-Ai/PipeLine/ConnectBg.webp";
 
-function page() {
-    return (
-        <>
-            <Future />
-            <Why />
-            <What />
-            <Use />
-            <section className="banner_light">
-                <BannerSection
-                    backgroundImage={BannerwithButtonBg}
-                    title={
-                        <>
-                            Top 5 Signs Your Tech Stack is Aging Out
-                        </>
-                    }
-                    buttonText="Read Blog"
-                    buttonLink="#"
-                />
-            </section>
-            <Business />
-            <How />
-            <Steps />
-            <div className="last_footer">
-                <FooterBg
-                    ContactImg={ContactImg}
-                    ButtonImage={ButtonImage}
-                    lightContent="Want to create a connected, intelligent, & resilient manufacturing ecosystem?
+const Page = () => {
+  return (
+    <>
+      <Future />
+      <Why />
+      <What />
+      <Use />
+      <section className="banner_light">
+        <BannerSection
+          backgroundImage={BannerwithButtonBg}
+          title={<>Top 5 Signs Your Tech Stack is Aging Out</>}
+          buttonText="Read Blog"
+          buttonLink="#"
+        />
+      </section>
+      <Business />
+      <How />
+      <Steps />
+      <div className="last_footer">
+        <FooterBg
+          ContactImg={ContactImg}
+          ButtonImage={ButtonImage}
+          lightContent="Want to create a connected, intelligent, & resilient manufacturing ecosystem?
 
                 "
-                    link="#"
-                />
-            </div>
-        </>
-    )
-}
+          link="#"
+        />
+      </div>
+    </>
+  );
+};
 
-export default page
+export default Page;
