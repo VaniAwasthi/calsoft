f2e9982d588828115aa3f8416d67432701891b39
zeesee123 <zubinchadha@gmail.com>
2025-08-22 16:41:29 +0530

    ai pred popups

--- app/data-ai/custom-llm-for-gen-ai-rag/page.js
+++ app/data-ai/custom-llm-for-gen-ai-rag/page.js
@@ -1,3 +1,4 @@
+"use client";
 import {
   AnalyticsSec,
   CustomBanner,
@@ -10,6 +11,8 @@ import { BannerSection } from "../../component/utilities/InfraSectorSec.jsx";
 import React from "react";
 import BannerwithButtonBg from "../../assets/Data-Ai/customllm/Knowmore.webp";
 import { ContactSecDataAi } from "../../component/utilities/ChallengeSolutionDataAi.jsx";
+import { showHubSpotForm } from "../../component/utilities/showHubSpotForm"; // import utility
+
 
 function page() {
   return (
@@ -29,6 +32,7 @@ function page() {
         }
         buttonText="Book a Meeting"
         buttonLink="#"
+        onButtonClick={() => showHubSpotForm("book-a-meeting")}
       />
       <EnterpriseSec />
       <SystemSec />
--- app/data-ai/predictive-orchestration/page.js
+++ app/data-ai/predictive-orchestration/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import {
   DrivenBanner,
@@ -10,6 +11,7 @@ import {
 import { BannerSection } from "../../component/utilities/InfraSectorSec.jsx";
 import BannerwithButtonBg from "../../assets/Data-Ai/dataDriven/KnowMore.webp";
 import { ContactSecDataAi } from "../../component/utilities/ChallengeSolutionDataAi.jsx";
+import { showHubSpotForm } from "../../component/utilities/showHubSpotForm"; // import utility
 
 function page() {
   return (
@@ -29,6 +31,7 @@ function page() {
         }
         buttonText="Book a Meeting"
         buttonLink="#"
+        onButtonClick={() => showHubSpotForm("book-a-meeting")}
       />
       <InferentialSec />
       <ModelSec />
